ccd47196e2e6266341d8b89419a39dec1aacbb7d
deepakk-cyber <deepak.k@suntek.ai>
2026-06-20 08:43:27 +0530

    added: analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +28,21 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="keywords" content="resume, customizer, ai, job description, customization" />
+        <meta name="author" content="Resume Customizer" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="bingbot" content="index, follow" />
+        <meta name="yandexbot" content="index, follow" />
+        <meta name="sitemap" content="https://resume-customizer.com/sitemap.xml" />
+        
+
+        <Analytics />
+      </head>
+      <body className="min-h-full flex flex-col">{children}
+      </body>
     </html>
   );
 }
